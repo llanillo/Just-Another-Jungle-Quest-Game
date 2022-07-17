670fcfda943d0d84333cf84b0fb0f2f21c932c49
Octavio Llanillo <llanillo4@hotmail.com>
2022-07-17 16:27:27 -0300

    Add player animation script

---
+++ Scripts/Player/Controllers/Animation/PlayerAnimation.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+namespace Justanotherjunglequestgame.Scripts.Player.Controllers.Animation
+{
+    public class PlayerAnimation : Node
+    {
+        private const string IdleAnim = "Idle";
+        private const string WalkAnim = "Walk";
+        private const string RunAnim = "Run";
+        private const string JumpAnim = "Jump";
+    
+        private AnimatedSprite _animatedSprite;
+    
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            _animatedSprite = GetNode<AnimatedSprite>("../../AnimatedSprite");
+        }
+
+        public void PlayAnimations(bool isOnFloor, Vector2 inputVelocity)
+        {
+            if (inputVelocity.x >= 1)
+            {
+                _animatedSprite.Play(RunAnim);
+                _animatedSprite.FlipH = false;
+            }
+            else if (inputVelocity.x <= -1)
+            {
+                _animatedSprite.Play(RunAnim);
+                _animatedSprite.FlipH = true;
+            }
+            else 
+            {
+                _animatedSprite.Play(IdleAnim);
+            }
+
+            if (!isOnFloor)
+            {
+                _animatedSprite.Play(JumpAnim);
+            }
+        }
+    }
+}
--- Scripts/Player/Controllers/Manager/PlayerManager.cs
+++ Scripts/Player/Controllers/Manager/PlayerManager.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Justanotherjunglequestgame.Scripts.Player.Controllers.Animation;
 using Justanotherjunglequestgame.Scripts.Player.Controllers.Input;
 using Justanotherjunglequestgame.Scripts.Player.Controllers.Movement;
 
@@ -7,19 +8,23 @@ namespace Justanotherjunglequestgame.Scripts.Player.Controllers.Manager
     public class PlayerManager : KinematicBody2D
     {
         private static readonly Vector2 FloorNormal = Vector2.Up;
-        private PlayerInput _playerInput;
+        
+        private PlayerAnimation _playerAnimation;
         private PlayerMovement _playerMovement;
+        private PlayerInput _playerInput;
     
         public override void _Ready()
         {
-            _playerInput = GetNode<PlayerInput>("Controllers/PlayerInput");
+            _playerAnimation = GetNode<PlayerAnimation>("Controllers/PlayerAnimation");
             _playerMovement = GetNode<PlayerMovement>("Controllers/PlayerMovement");
+            _playerInput = GetNode<PlayerInput>("Controllers/PlayerInput");
         }
 
         public override void _PhysicsProcess(float delta)
         {
             var inputVelocity = PlayerInput.OnMovementInput();
             var velocity = MoveAndSlide(_playerMovement.GetMoveVelocity(IsOnFloor(), inputVelocity), FloorNormal);
+            _playerAnimation.PlayAnimations(IsOnFloor(), inputVelocity); // Plays the corresponding animation
             _playerMovement.Velocity = velocity; // Updates the current velocity
         }
 
